81cb8ace183b4f7afd853d7d1720122d10278cd9
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-03-22 18:23:57 -0500

    sorting and onLoad

--- src/index.js
+++ src/index.js
@@ -25,23 +25,26 @@ const object = [
 
 const objectContainer = document.getElementById('object');
 
-object.forEach((object) => {
-  const list = document.createElement('li');
-  const div2 = document.createElement('div');
-  const task = document.createElement('p');
-  const check = document.createElement('input');
-  const menu = document.createElement('i');
-  div2.classList.add('div2');
-  menu.classList.add('fa-solid');
-  menu.classList.add('fa-ellipsis-vertical');
-  list.classList.add('object-li');
-  task.innerHTML = object.description;
-  task.classList.add('task');
-  check.classList.add('check');
-  check.type = 'checkbox';
-  div2.appendChild(check);
-  div2.appendChild(task);
-  list.appendChild(div2);
-  list.appendChild(menu);
-  objectContainer.appendChild(list);
-});
+window.onload = () => {
+  object.sort((x, y) => x.index - y.index);
+  object.forEach((object) => {
+    const list = document.createElement('li');
+    const div2 = document.createElement('div');
+    const task = document.createElement('p');
+    const check = document.createElement('input');
+    const menu = document.createElement('i');
+    div2.classList.add('div2');
+    menu.classList.add('fa-solid');
+    menu.classList.add('fa-ellipsis-vertical');
+    list.classList.add('object-li');
+    task.innerHTML = object.description;
+    task.classList.add('task');
+    check.classList.add('check');
+    check.type = 'checkbox';
+    div2.appendChild(check);
+    div2.appendChild(task);
+    list.appendChild(div2);
+    list.appendChild(menu);
+    objectContainer.appendChild(list);
+  });
+};
