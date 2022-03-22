a7fe19a81031d57451d932f4c0c04264e98d919f
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-03-22 17:56:01 -0500

    list structure done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-import _ from 'lodash';
 import './style.css';
 
-function toDoList() {
-  const element = document.createElement('div');
+const object = [
+  {
+    checkbox: false,
+    description: 'first activity',
+    index: 1,
+  },
+  {
+    checkbox: false,
+    description: 'second activity',
+    index: 2,
+  },
+  {
+    checkbox: false,
+    description: 'third activity',
+    index: 3,
+  },
+  {
+    checkbox: false,
+    description: 'fourth activity',
+    index: 4,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack', 'this is my name'], ' ');
-  element.classList.add('hello');
-  return element;
-}
+const objectContainer = document.getElementById('object');
 
-document.body.appendChild(toDoList());
+object.forEach((object) => {
+  const list = document.createElement('li');
+  const div2 = document.createElement('div');
+  const task = document.createElement('p');
+  const check = document.createElement('input');
+  const menu = document.createElement('i');
+  div2.classList.add('div2');
+  menu.classList.add('fa-solid');
+  menu.classList.add('fa-ellipsis-vertical');
+  list.classList.add('object-li');
+  task.innerHTML = object.description;
+  task.classList.add('task');
+  check.classList.add('check');
+  check.type = 'checkbox';
+  div2.appendChild(check);
+  div2.appendChild(task);
+  list.appendChild(div2);
+  list.appendChild(menu);
+  objectContainer.appendChild(list);
+});
